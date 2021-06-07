89e67240645069811fba5cc7e318d98219d7b5e2
njiang6 <79889759+njiang6@users.noreply.github.com>
2021-06-06 21:05:13 -0700

    Update custom_automation.py

--- custom_automation.py
+++ custom_automation.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 
-chrome_driver_path = "/Users/nathanj/Development/chromedriver"
+chrome_driver_path = "/Users/user/Development/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 list_of_urls = [
-    {"url": "https://www.amazon.com/Ethernet-Development-ARDUINO-ETHERNET-SHIELD/dp/B011PCMMM0",
+    {"url": "https://www.amazon.com/product",
      "lowest_price": 25,
-     "name": "Arduino Ethernet Shield"}]
+     "name": "Product"}]
 for group in list_of_urls:
     driver.get(group["url"])
 
